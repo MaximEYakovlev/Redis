a0a4305be9eb62f2ffa3dda7a040fa6c964f3a4d
Maxim Yakovlev <it.trainee@yandex.ru>
2022-11-12 17:35:48 +0300

    installed redis

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const Redis = require("redis");
+const redisClient = Redis.createClient();
+
+const DEFAULT_EXPIRATION = 3600;
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/photos", async (req, res) => {
@@ -11,6 +16,7 @@ app.get("/photos", async (req, res) => {
     "https://jsonplaceholder.typicode.com/photos",
     { params: { albumId } }
   );
+  redisClient.setEx("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
   res.json(data);
 });
 
